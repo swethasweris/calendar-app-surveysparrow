76acc06da6c3176b49818d8667aed0786167e362
SWETHA PRABAKARAN <swethap.22it@kongu.edu>
2025-07-21 11:37:33 +0530

    coded

--- calendar/src/components/DateDetails.jsx
+++ calendar/src/components/DateDetails.jsx
@@ -4,45 +4,48 @@ import { useParams, useNavigate } from 'react-router-dom';
 export default function DateDetails({ events, setEvents }) {
   const { date } = useParams();
   const navigate = useNavigate();
-
   const filtered = events.filter(ev => ev.date === date);
 
-  const handleEdit = (event) => {
-    const newTitle = prompt("Edit title", event.title);
-    if (!newTitle) return;
-    const newTime = prompt("Edit time", event.time);
-    if (!newTime) return;
+  const handleEdit=(event)=>{
 
-    const conflict = events.some(ev => ev.id !== event.id && ev.date === date && ev.time === newTime);
+    const newTitle=prompt("enter title",event.title);
+    if(!newTitle) return;
+    const newTime=prompt("enter title",event.time);
+    if(!newTime) return;
+    
+    const conflict=events.some(ev=>ev.id!==event.id&&ev.time===newTime);
 
-    const updated = events.map(ev =>
-      ev.id === event.id ? { ...ev, title: newTitle, time: newTime, conflict } : ev
-    );
+    const updatedEvent=events.map((ev)=>(ev.id===event.id?{...ev,title:newTitle,time:newTime,conflict}:ev));
 
-    setEvents(updated);
+    setEvents(updatedEvent);
+
   };
 
-  const handleDelete = (id) => {
-    const confirmDel = window.confirm("Delete this event?");
-    if (!confirmDel) return;
-    setEvents(events.filter(ev => ev.id !== id));
+  const handleDel=(id)=>{
+    const val=window.confirm("are you sure you want to delete?");
+    if(!val) return;
+    const updatedEvent=events.filter(ev=>ev.id!==id);
+    setEvents(updatedEvent);
+
     
   };
 
+  
   return (
     <div className="event-details">
       <h3>Events on {date}</h3>
       {filtered.map(ev => (
         <div key={ev.id} className={`event ${ev.conflict ? 'conflict' : ''}`}>
           {ev.time} - {ev.title}
-          < div className="event-actions">
-          <button onClick={() => handleEdit(ev)}>Edit</button>
-          <button onClick={() => handleDelete(ev.id)}>Delete</button>
+          <div className="event-actions">
+            <button className="edit" onClick={()=>handleEdit(ev)}>EDIT</button>
+            <button className="del" onClick={()=>handleDel(ev.id)}>DELETE</button>
           </div>
-        </div>
+          </div>
       ))}
       <div className="back">
-      <button onClick={() => navigate('/')}>Back</button></div>
+        <button onClick={() => navigate('/')}>Back</button>
+      </div>
     </div>
   );
 }
